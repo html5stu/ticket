af37500f81e7b765d6d55a1333dc20bcc65fbc94
修改index.添加底部导航 <384868059@qq.com>
2018-02-02 14:52:27 +0800

    机选一注和机选5注完成

---
+++ js/index.js
@@ -0,0 +1,168 @@
+		mui.init({
+				
+			});
+			//循环遍历红色球数字
+			var redArr=[];
+			for(var i=1;i<34;i++){
+				redArr.push(i)
+			}
+			$(redArr).each(function(index,value){
+				var li=$("<li>"+(index+1)+"</li>")
+				$(".red").append(li)
+			})
+			
+			//循环遍历蓝色球数字
+			var blueArr=[];
+			for(var i=1;i<17;i++){
+				blueArr.push(i)
+			}
+			$(blueArr).each(function(index,value){
+				let Li=$("<li>"+(index+1)+"</li>");
+				$(".blue").append(Li);
+			});
+			//计算排列组合
+			 function combination(m,n){   //计算排列组合
+			        return factorial(m,n)/factorial(n,n);  
+			        //就是Cmn(上面是n，下面是m) = Amn(上面是n，下面是m)/Ann(上下都是n)
+			    }  
+		    //计算阶乘
+		    function factorial(m,n){  
+		        var num = 1;  
+		        var count = 0;  
+		        for(var i = m;i > 0;i--){  
+		            if(count == n){		//当循环次数等于指定的相乘个数时，即跳出for循环  
+		                break;  
+		            }  
+		            num = num * i;  
+		            count++;  
+		        }  
+		        return num;  
+		    }  
+			//计算多少注多少钱
+			function calculate(){
+				var redActiveLen=$(".red").find("li.active").length;
+				var blueActiveLen=$(".blue").find("li.active").length;
+				if(redActiveLen<6){
+					$(".Betting").text(0);
+					$(".money").text(0);
+					return false;
+				}else{
+					var betting=Number(combination(redActiveLen,6)*(blueActiveLen));
+					$(".Betting").text(betting);
+					var money=Number(betting*2);
+					$(".money").text(money);
+				}
+			}
+			//把产生随机数的数组挂载到原型上
+			Array.prototype.in_array = function(element) {
+				for(var i = 0; i < this.length; i++) {
+					if(this[i] == element) {
+						return true;
+					}
+				}
+				return false;
+			}
+			
+			let btn = document.querySelector(".com-option");
+			let lis = document.querySelectorAll(".red li");
+			let bluelis=document.querySelectorAll(".blue li");
+			let redActive = [],
+				blueActive = [];
+			
+			//点击机选清除掉之前的所有被选中的数字
+			function deleteALlnumber(){
+				let ball_list=document.getElementsByClassName('ball-list')[0];
+				let Li_list=ball_list.getElementsByTagName('li');
+				let $Li_list=$(Li_list);
+				$Li_list.removeClass("active");
+			}
+//			产生随机数方法
+			function  produceRandom(){
+				removeClass();
+				redActive = [];
+				blueActive = [];
+				for(let i = 0; redActive.length < 6; i++) {
+					let radom = Math.random() * 33;
+					let redItem = Math.floor(radom);
+					if(!redActive.in_array(redItem)) {
+						redActive.push(redItem);
+					}
+				}
+				for(let i = 0; blueActive.length < 1; i++) {
+					let radom = Math.random() * 16;
+					let blueItem = Math.floor(radom);
+					if(!blueActive.in_array(blueItem)) {
+						blueActive.push(blueItem);
+					}
+				}
+			}
+		
+			//点击机选产生一组随机数
+			btn.onclick = function () {
+				deleteALlnumber();
+				produceRandom();
+				addClass();
+				calculate();//调用 计算多少注多少钱的方法
+					
+			}
+		//给机选选中的随机数添加active类
+		function addClass() {
+			for (let i of redActive) {
+				lis[i].classList.add("active");
+			}
+
+			for (let i of blueActive) {
+				bluelis[i].classList.add("active");
+			}
+		}
+		//每次点击机选把之前选中的随机数清除
+		function removeClass() {
+			for (let i of redActive) {
+				lis[i].classList.remove("active");
+			}
+			for (let i of blueActive) {
+				bluelis[i].classList.remove("active");
+			}
+		}
+		//计算多少注   多少钱
+			$(".ball-list").find("li").click(function(){
+				$(this).toggleClass("active");
+				calculate();
+			})
+		//点击删除按钮，清除所选号码
+		$(".mui-icon-trash").click(function(){
+			deleteALlnumber();
+			calculate();
+			document.querySelector('.Betting').innerHTML=0;
+			document.querySelector('.money').innerHTML=0;
+			
+		})
+		let btn_sure=document.getElementById("btn-sure");
+		btn_sure.onclick=function(){
+				let beet=document.querySelector('.Betting').innerHTML;
+				let ALlmoney=document.querySelector('.money').innerHTML;
+				let redActiveNum=document.querySelectorAll('.red .active');
+				let blueActiveNum=document.querySelectorAll('.blue .active');
+				var redActiveArr=[];
+				var blueActiveArr=[];
+				for (let i in redActiveNum){
+					redActiveArr.push(redActiveNum[i].innerHTML)
+				}
+				let redArray=redActiveArr.splice(0,redActiveArr.length-6);//选中的红色球数组
+			    let redStr=redArray.join();//红色球字符串
+				for (let i in blueActiveNum){
+					blueActiveArr.push(blueActiveNum[i].innerHTML)
+				}
+				let blueArray=blueActiveArr.splice(0,blueActiveArr.length-6);//选中的蓝色球数组
+			    let blueStr=blueArray.join();//蓝色球字符串
+			    if(redActiveNum.length<6){
+						mui.alert("请至少选择6个红球",'',function(){
+						})
+						return;
+					}else if(blueActiveNum.length<1){
+						mui.alert("请至少选择1个蓝球",'',function(){
+						})
+						return;
+					}
+//				window.location.href="bettinglist.html?redStr="+redStr+"&blueStr="+blueStr+"&beet="+beet+"&ALlmoney="+ALlmoney;	
+			}
---
+++ js/select.js
@@ -0,0 +1,96 @@
+	
+	
+	//机选一注
+	
+   	var selectOne=$(".selectOne");
+	selectOne.click(function(){
+		 var reds=[parseInt(Math.random()*33+1)]; //1-33的随机数
+			  //定义一个数组 用来装蓝色球的随机数
+			  var blues=[parseInt(Math.random()*16+1)]; //1-16的随机数
+		      while(reds.length<6){  //双色球是6红1蓝，故长度小于6 说明会循环5次 因为前面reds已经有一次的默认值
+		        var r=parseInt(Math.random()*33+1); //定义r 随机数的取值与reds一致 用于与reds作比较
+		        for (var i = 0; i < reds.length; i++) {
+		          if (reds[i]==r) {    //若 reds中的随机值与r中的相等，则跳出，继续i++取值
+		            break;        //若reds中的随机值与r中的不相等，则进入else if
+		          } else if(i==reds.length-1){ //reds.length-1 --> 原先的reds.length为5，减1为4，而数组是从0 开始算，所以这里会循环5次，刚好与 reds.length<6 同步；且 i等于reds.length-1，是让随机数循环5次（另外就是让它找到定义中的最后一位数）
+		            reds.push(r);        //将r中的值添加到reds中  push()：向数组中添加元素
+		//值得注意是！r与reds作比较时，是以reds中现有的数作比较，因为 reds.push(r); 这句代码已经向reds中添加了数值，所以r不是以reds中自身产生的随机数作比较，而是原先r添加到reds中的值作比较
+		          }
+		        }
+		      }
+	      //排序
+	      reds.sort(function compare(a,b) {
+	        return a-b;
+	      })
+			 var OneArray=reds.concat(blues);
+      		var ul=$("<ul class='mui-table-view'></ul>");
+      		var contentlist=$(".content-list");
+      		var $li_01='<li class="mui-table-view-cell icon iconfont icon-2guanbi color-gray"></li>';
+      			ul.append($li_01);
+      			contentlist.append(ul)
+      		$(OneArray).each(function(index,value){
+      			var val=OneArray[index];
+      			console.log(val)
+      			var $li='<li class="mui-table-view-cell color-red">'+val+'</li>';
+      			ul.append($li);
+      			contentlist.append(ul)
+      		})
+	})
+	
+	//机选五注
+		var selectFive=$(".selectFive");
+		$(".selectFive").click(function(){
+				var a=random();
+				var b=random();
+				var c=random();
+				var d=random();
+				var e=random();
+				var concatArr=[];
+				concatArr.push(a,b,c,d,e);
+				var contentlist=$(".content-list");
+				$(concatArr).each(function(index,value){
+					var val=concatArr[index];
+					console.log(val)
+					var ul=$("<ul class='mui-table-view'></ul>");
+					ul.append('<li class="mui-table-view-cell icon iconfont icon-2guanbi color-gray">'+'</li>'+
+					'<li class="mui-table-view-cell color-red">'+val[0]+'</li>'+
+					'<li class="mui-table-view-cell color-red">'+val[1]+'</li>'+
+					'<li class="mui-table-view-cell color-red">'+val[2]+'</li>'+
+					'<li class="mui-table-view-cell color-red">'+val[3]+'</li>'+
+					'<li class="mui-table-view-cell color-red">'+val[4]+'</li>'+
+					'<li class="mui-table-view-cell color-red">'+val[5]+'</li>'+
+					'<li class="mui-table-view-cell color-blue">'+val[6]+'</li>'
+					)
+					contentlist.append(ul);
+				})
+			})
+	//把产生随机数的数组挂载到原型上
+		Array.prototype.array = function(element) {
+			for(var i = 0; i < this.length; i++) {
+				if(this[i] == element) {
+					return true;
+				}
+			}
+			return false;
+		}
+	function random(){
+		redActive = [];
+		blueActive = [];
+		for(let i = 0; redActive.length < 6; i++) {
+			let radom = Math.random() * 33+1;
+			let redItem = Math.floor(radom);
+			if(!redActive.array(redItem)) {
+				redActive.push(redItem);
+			}
+		}
+		for(let i = 0; blueActive.length < 1; i++) {
+			let radom = Math.random() * 16+1;
+			let blueItem = Math.floor(radom);
+			if(!blueActive.array(blueItem)) {
+				blueActive.push(blueItem);
+			}
+		}	
+		var FiveArray=redActive.concat(blueActive);
+		return FiveArray;	
+	}
+	
